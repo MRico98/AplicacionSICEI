06459a92ec0962ac9ca2be1ab8de7b4d32054014
DiegoAvmor <diegoavmor@gmail.com>
2021-01-12 15:41:20 -0600

    No se que pedo

--- src/main/java/mx/uady/sicei/service/EmailService.java
+++ src/main/java/mx/uady/sicei/service/EmailService.java
@@ -2,29 +2,82 @@ package mx.uady.sicei.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.SimpleMailMessage;
-import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
+import mx.uady.sicei.util.GmailAPI;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.security.GeneralSecurityException;
+import java.util.Properties;
+
+import javax.mail.MessagingException;
+import javax.mail.Session;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
+import com.google.api.client.util.Base64;
+import com.google.api.services.gmail.Gmail;
+import com.google.api.services.gmail.model.Message;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 @Service
 public class EmailService {
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(EmailService.class);
-
     @Autowired
-    private JavaMailSender javaMailSender;
+    private GmailAPI gmailAPI;
 
     @Async
     public void sendWelcome(String recepient, String userName) {
-        SimpleMailMessage msg = new SimpleMailMessage();
-        msg.setTo(recepient);
-        msg.setSubject("Welcome to insecurity " + userName);
-        msg.setText("Haha php goes brrrr <br>");
+        Gmail gmail;
+        try {
+            gmail = gmailAPI.getGmailService();
+            sendMessage(gmail, userName);
+        } catch (IOException | GeneralSecurityException | MessagingException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
 
-        javaMailSender.send(msg);
-        LOGGER.info("Mail enviado!");
+    public Message sendMessage(Gmail service, String userEmail) throws MessagingException, IOException {
+        MimeMessage emailContent = createEmail(userEmail, "p4480707@gmail.com", "Welcome To Insanity", "bodyText");
+        Message message = createMessageWithEmail(emailContent);
+        message = service.users().messages().send(userEmail, message).execute();
+
+        System.out.println("Message id: " + message.getId());
+        System.out.println(message.toPrettyString());
+        return message;
+    }
+    
+    private Message createMessageWithEmail(MimeMessage emailContent)
+            throws MessagingException, IOException {
+        ByteArrayOutputStream buffer = new ByteArrayOutputStream();
+        emailContent.writeTo(buffer);
+        byte[] bytes = buffer.toByteArray();
+        String encodedEmail = Base64.encodeBase64URLSafeString(bytes);
+        Message message = new Message();
+        message.setRaw(encodedEmail);
+        return message;
+    }
+
+    private MimeMessage createEmail(String to,
+                                          String from,
+                                          String subject,
+                                          String bodyText)
+            throws MessagingException {
+        Properties props = new Properties();
+        Session session = Session.getDefaultInstance(props, null);
+
+        MimeMessage email = new MimeMessage(session);
+
+        email.setFrom(new InternetAddress(from));
+        email.addRecipient(javax.mail.Message.RecipientType.TO,
+                new InternetAddress(to));
+        email.setSubject(subject);
+        email.setText(bodyText);
+        return email;
     }
     
 }
